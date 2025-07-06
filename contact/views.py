e8bb0da70c0f3e5a4c9738d6290a6f9c79eec875
Cassiano Shigueyuki Nishikawa <csnishikawa@gmail.com>
2025-07-06 14:56:27 -0300

    urls files configs

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """
+    Render the contact page.
+    """
+    return render(
+        request,
+        'contact/index.html'
+    )
