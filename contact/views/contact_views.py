0a26d8038b75dd74088e51a93ac6e56a088cfb50
Cassiano Shigueyuki Nishikawa <csnishikawa@gmail.com>
2025-07-08 02:22:14 -0300

    Adding pagination

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
-from django.shortcuts import get_object_or_404
 
 
 def index(request):
@@ -9,11 +10,49 @@ def index(request):
     """
     contacts = Contact.objects \
         .filter(show=True)\
-        .order_by('-id')[0:10]
+        .order_by('-id')
+
+    # Pagination
+    paginator = Paginator(contacts, 10)  # Show 10 contacts per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contacts - ',
+    }
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+
+    # Pagination
+    paginator = Paginator(contacts, 10)  # Show 10 contacts per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,
+        'site_title': 'Contacts - ',
+        'search_value': search_value,
     }
     return render(
         request,
