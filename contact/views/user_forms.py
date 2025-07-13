d88534e713e2ff0baa9e5984c3249ee6067e4ed9
Cassiano Shigueyuki Nishikawa <csnishikawa@gmail.com>
2025-07-13 02:59:28 -0300

    Implement user authentication features: add login, logout, and user update functionality; update header navigation; create user forms.

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from contact.forms import RegisterForm
+from django.contrib import messages, auth
+from django.contrib.auth.forms import AuthenticationForm
+from contact.forms import RegisterForm, RegisterUpdateForm
 
 
 def register(request):
@@ -21,3 +22,62 @@ def register(request):
             'form': form
         }
     )
+
+
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
+
+    if request.method != 'POST':
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+
+    form = RegisterUpdateForm(
+        data=request.POST,
+        instance=request.user
+    )
+
+    if not form.is_valid():
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+
+    form.save()
+    messages.success(request, 'User updated')
+    return redirect('contact:user_update')
+
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, 'Successfuly Login')
+            return redirect('contact:index')
+
+        messages.error(request, 'Invalid Login')
+
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form
+        }
+    )
+
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
